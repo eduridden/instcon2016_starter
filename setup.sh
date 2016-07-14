153cfac19a51d2c3968eeb475288a13f3863787c
K Developer <kajigga@gmail.com>
2016-07-13 22:30:52 -0400

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/kajigga/instcon2016-ltisession.git $HOME/workspace/instcon2016
+cd $HOME/workspace/instcon2016
+git checkout tst_step1
+echo "LTI Repository Cloned"
+virtualenv $HOME/workspace/env
+source $HOME/workspace/env/bin/activate
+pip install -r $HOME/workspace/instcon2016/requirements.txt
+echo "virtualenv created"
